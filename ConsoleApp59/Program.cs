86c48b92ed76ac069b12a54e0cb6c8d2068276b3
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-03-21 16:13:30 +0100

    Add projects.

--- ConsoleApp59/Program.cs
+++ ConsoleApp59/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Nuget;
+
 Console.WriteLine("Hello, World!");
+for (int i = 0; i < 3; i++)
+{
+    if (i == 2)
+    {
+        new Nuget.Nuget1().Test();
+    }
+}
