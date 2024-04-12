fc51d7831a2d3f1c82755eafa81cacd3790d53aa
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-04-12 10:24:59 +0200

    Add framework project.

---
+++ ConsoleApp_Framework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib_Framework;
+
+namespace ConsoleApp_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.InsertPerson(person);
+        }
+    }
+}
